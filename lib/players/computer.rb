83ccfee6a4249bcbbd086653fa99b7f7481e5882
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-16 23:08:15 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,9 +2,11 @@ module Players
   class Computer < Player
 
   def move(board) 
-    if @board.valid_move? 
-    rand(9).to_s
-    end
+    if board.valid_move? 
+      
+   
+   #rand(9).to_s
+   #end
  
   end
 end

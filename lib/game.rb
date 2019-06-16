4bdaf25f7a40434e6535e169e50b42a98763ae0f
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-16 21:12:15 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -21,10 +21,9 @@ class Game
  def won?
   WIN_COMBINATIONS.detect { |combo| 
     @board.cells[combo[0]] == @board.cells[combo[1]] && 
-    @board.cells[combo[1]] == @board.cells[combo[2]]
-    
+    @board.cells[combo[1]] == @board.cells[combo[2]] 
+    && @board.taken?(combo[0] + 1)
   }
-  
   end
   
   def winner

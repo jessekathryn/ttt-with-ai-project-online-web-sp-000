ad36ef05bd80c15c9abbefcfa9ebef189ad06ffd
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-16 22:16:48 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,7 +61,7 @@ class Game
   
   def turn 
     puts "Please enter 1-9:"
-      #user_input = gets.strip
+      user_input = gets.chomp
       index = input_to_i(user_input) 
         if valid_move?(index)
           move(index, current_player) 

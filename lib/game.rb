eb38548379ab38a9da9e12a5a82eaaa485d836fa
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-16 22:13:24 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -60,6 +60,16 @@ class Game
   end 
   
   def turn 
+    puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input) 
+    if valid_move?(index)
+    move(index, current_player) 
+  else 
+    turn
+  end
+    display_board
+end
   end
 
 end
